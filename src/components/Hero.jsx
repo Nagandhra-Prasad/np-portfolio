a2867132a428daac1ea646e4bfbcf4cb24035886
Nagandhra Prasad <selvalaksmi758@gmail.com>
2024-06-24 14:48:57 +0530

    deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from '../constants'
-import profilePic from "../assets/pic.jpg"
+import profilePic from "../assets/IMG_7603 2.png"
 import { delay, motion } from "framer-motion"
 
 const container =(delay)=>({
