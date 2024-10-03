09fc0f215456c3fbc4d9a6e7380735a80d2bf5e3
NagandhraPrasad <ethan347winters@gmail.com>
2024-10-03 17:04:45 +0530

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from "../assets/about.jpg"
+import aboutImg from "../assets/about.png"
 import { ABOUT_TEXT } from "../constants"
 import {  motion } from "framer-motion"
 
@@ -15,12 +15,12 @@ const About = () => {
         className=' flex flex-wrap'>
             <div className=' w-full lg:w-1/2 lg:p-8'>
             <div className='flex items-center justify-center '>
-            <img className=" rounded-2xl"  src={aboutImg} alt="about" />
+            <img className=" rounded-3xl h-80 my-auto "  src={aboutImg} alt="about" />
             </div>
             </div>
             <motion.div className=" w-full lg:w-1/2">
                 <div className="flex justify-center lg:justify-start">
-                <p className=" my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+                <p className=" my-12 max-w-xl py-6">{ABOUT_TEXT}</p>
                 </div>
             </motion.div>
         </motion.div>
